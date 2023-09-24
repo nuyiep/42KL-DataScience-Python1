b51cafe763da608ddb12c46650a4afa23d04435e
Pei <nuyiep@hotmail.com>
2023-09-24 23:28:59 +0800

    ex01- error checking done

--- ex01/array2D.py
+++ ex01/array2D.py
@@ -1,5 +1,28 @@
 import numpy as np
 
+def error_handling(family: list, start: int, end: int) -> list:
+	'''
+		Check if lists are not a list & if are the same size
+	'''
+	if (isinstance(family, list) == False):
+		print("Error: List are not a list. It's a ", end='')
+		print(type(family))
+		exit()
+	first_column_length = len(family[0])
+	equal_row_length = True
+	for row in family:
+		if (len(row) != first_row_length):
+			equal_row_length = False
+			break
+	if equal_row_length == False:
+		print("Error: List rows are not of equal length")
+		exit()
+
+# convert 2-D array list into a numpy 2-d array
+# numpy 2-D array list must have the same row and column length
 def slice_me(family: list, start: int, end: int) -> list:
-	print("My shape is : ")
+	error_handling(family, start, end)
+	np_family = np.array(family)
+	print("My shape is : ", end='')
+	print(np.shape(np_family))
 	print("My new shape is: ")
