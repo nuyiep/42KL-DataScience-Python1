09d661420cf4b2786a6988b3f4debdd86b26a854
Pei Yun Lau <plau@u91z08s01.42kl.edu.my>
2023-09-25 16:17:13 +0800

    ex02 done

---
+++ ex02/tester.py
@@ -0,0 +1,8 @@
+
+from load_image import ft_load
+
+print(ft_load("landscape.jpg"))
+print(ft_load("landscape.jpeg"))
+print(ft_load("animal.jpeg"))
+print(ft_load("kitty-puss.jpg"))
+print(ft_load("Doesnt exist"))
